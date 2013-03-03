41d4cfa909e6ab6494adaf68f74e126c22e668bb
Huaiyu Wu <huaiyu.wu2010@gmail.com>
2013-03-03 04:54:27 -0800

    more scoring functions added

--- prototype/utils/views.py
+++ prototype/utils/views.py
@@ -25,7 +25,7 @@ def integrate_linear(list):
 # Given a list of values for a function, approximates an integral for the square of the function over the given range.
 # The point of being able to integrate the square of the function is that, because the function y=x^2 is convex,
 # we can use the integral of a quadratic equation to more cleanly penalize higher values over lower values.
-# Examples of this (TODO for Nick, implement these): Braking pressure, pedal force, air conditioning.
+# Examples of this: Braking pressure, pedal force, air conditioning.
 def integrate_quadratic(list):
     if len(list) == 1:
         return list[0] * list[0]
@@ -35,6 +35,46 @@ def integrate_quadratic(list):
         b = list[i+1] - list[i]
         ret = ret + (a * (a + b)) + (b * b) / 3.
     return ret
+
+# Given a list of values, returns the average value of the function.
+def average_linear_value(list):
+    return integrate_linear(list) / len(list)
+
+# Given a list of values, returns the average value of the square of the function,
+# after normalizing for length of the trip. 
+def average_quadratic_value(list):
+    return integrate_quadratic(list) / (len(list) * len(list))
+
+WEIGHTING_FACTOR = 1
+# TODO (Nick): The following four functions are all currently unweighted, and
+# are likely to produce very high scores in their current form. We may decide
+# to increase the penalty consequently if it is remarkably hard to get a low
+# score. This can be done by increasing the WEIGHTING_FACTOR. Determining a
+# good value of the WEIGHTING_FACTOR constant is blocked on observing data.
+
+# This function will return a score from 0 to 100 indicating how much excessive
+# pedal force is being applied.
+# Score will be a real number between 0 and 100.
+def score_pedal_force(pedal_force_list):
+    return max(0.0, 100 - WEIGHTING_FACTOR * WEIGHTING_FACTOR * average_quadratic_value(pedal_force_list))
+
+# This function will return a score from 0 to 100 indicating how much excessive
+# braking pressure is being applied.
+# Score will be a real number between 0 and 100. 
+def score_braking_pressure(braking_pressure_list):
+    return max(0.0, 100 - WEIGHTING_FACTOR * average_quadratic_value(braking_pressure_list))
+    
+# This function will return a score from 0 to 100 indicating how much excessive
+# air conditioning is being used.
+# Score will be a real number between 0 and 100.    
+def score_air_conditoning(air_conditoning_list):
+    return max(0.0, 100 - WEIGHTING_FACTOR * average_quadratic_value(air_conditoning_list))
+    
+# This function will return a score from 0 to 100 indicating how intense the
+# sun is when driving (more intense sunlight leads to less economical driving)
+# Score will be a real number between 0 and 100.    
+def score_sun_intensity(sun_intensity_list):
+    return max(0.0, 100 - WEIGHTING_FACTOR * average_quadratic_value(sun_intensity_list))
 
 OPTIMAL_SPEED = 60 # TODO (Nick): Verify that units of speed are miles per hour. Blocked on receiving data.
 # This function will return a score from 0 to 100 indicating how much excessive speeding
@@ -82,8 +122,8 @@ def score_idling(engine_speed_list):
 # ASSUMPTIONS: Positive gradient means uphill. Verifying this is blocked on seeing data.
 # TODO (Nick): Make the formula more advanced than a simple dot product. Determine
 # an appropriate lookahead range and more strongly penalize when drivers do not
-# approach gradients properly. This may be blocked on doing an actual drive ourselves
-# to see what is reasonable for lookahead range.
+# approach gradients properly. Blocked on receiving data, potentially blocked on
+# actually doing a drive ourselves to test lookahead values manually.
 def score_thinking_ahead(pedal_force_list, gradient_list):
     assert len(pedal_force_list) == len(gradient_list)
     total_gradient = 0
@@ -94,5 +134,4 @@ def score_thinking_ahead(pedal_force_list, gradient_list):
         total_force = total_force + pedal_force_list[i]
         total_gradient = total_gradient + abs(gradient_list[i])
     return 100. * (1. - badness / (total_force * 1. * total_gradient))
-
 
