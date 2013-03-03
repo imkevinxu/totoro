93f45e49d313e278959cdf9971ae448a7ce2c931
Huaiyu Wu <huaiyu.wu2010@gmail.com>
2013-03-03 14:12:33 -0800

    2 space tabs turned into 4 space tabs

--- prototype/simulations/generate_data.py
+++ prototype/simulations/generate_data.py
@@ -19,27 +19,27 @@ steering_wheel = [0]
 air_conditioning = [0]
 
 itera = 0
-while  True:
-  vs = (random.randint(0, MAX_SPEED) + vehicle_speed[-1]) / 2
-  if vs < MAX_SPEED/2:
-    vs = vs*(random.randint(0,MAX_SPEED/2)>vs)
-  vehicle_speed.append(vs)
+while True:
+    vs = (random.randint(0, MAX_SPEED) + vehicle_speed[-1]) / 2
+    if vs < MAX_SPEED/2:
+        vs = vs*(random.randint(0,MAX_SPEED/2)>vs)  
+    vehicle_speed.append(vs)
 
-  if random.randint(0,100) > 90:
-    ac = random.randint(-COND_BND, COND_BND)
-  else:
-    ac = air_conditioning[-1]
-  air_conditioning.append(ac)
+    if random.randint(0,100) > 90:
+        ac = random.randint(-COND_BND, COND_BND)
+    else:
+        ac = air_conditioning[-1]
+    air_conditioning.append(ac)
 
-  odo = odometer[-1] + vs
-  odometer.append(odo)
+    odo = odometer[-1] + vs
+    odometer.append(odo)
 
-  sw = random.normalvariate(0, 0.25)*MAX_STEER_ANGLE
+    sw = random.normalvariate(0, 0.25)*MAX_STEER_ANGLE
   
-  #print str(vs) + " " + str(ac) + " " + str(odo) + " " + str(sw)
-  json_pic = {"v":vs, "ac":ac, "odo":odo, "sw":sw}
-  print json.dumps(json_pic)
-  sys.stdout.flush()
-  json_dat.append(json_pic)
-  time.sleep(abs(random.normalvariate(0,0.5)))
-  itera = itera + 1
+    #print str(vs) + " " + str(ac) + " " + str(odo) + " " + str(sw)
+    json_pic = {"v":vs, "ac":ac, "odo":odo, "sw":sw}
+    print json.dumps(json_pic)
+    sys.stdout.flush()
+    json_dat.append(json_pic)
+    time.sleep(abs(random.normalvariate(0,0.5)))
+    itera = itera + 1
--- prototype/simulations/urls.py
+++ prototype/simulations/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-  url(r'^generate$', 'simulations.views.generate'),
-  url(r'^admin/', include(admin.site.urls)),
+    url(r'^generate$', 'simulations.views.generate'),
+    url(r'^admin/', include(admin.site.urls)),
 )
