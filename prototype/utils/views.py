3631c63b6c77dd137328361725d95adee6f699fb
Huaiyu Wu <huaiyu.wu2010@gmail.com>
2013-03-03 03:25:20 -0800

    engine idling score function added

--- prototype/utils/views.py
+++ prototype/utils/views.py
@@ -21,7 +21,8 @@ def integrate_linear(list):
     ret = ret - list[0] / 2.
     ret = ret - list[-1] / 2.
     return ret
-    
+
+# Given a list of values for a function, approximates an integral for the square of the function over the given range  
 def integrate_quadratic(list):
     if len(list) == 1:
         return list[0] * list[0]
@@ -31,3 +32,21 @@ def integrate_quadratic(list):
         b = list[i+1] - list[i]
         ret = ret + (a * (a + b)) + (b * b) / 3.
     return ret
+    
+IDLING_ENGINE_THRESHOLD = 1 # TODO (Nick): Make sure that this makes sense. Blocked on receiving data.
+# This function will return a score from 0 to 100 indicating how much idling took place.
+# Longer sequences of idling will be more strongly penalized than shorter bursts of idling.
+# Score will be a real number between 0 and 100
+def score_idling(engine_speed_list):
+    idling_penalty = 0
+    curr_streak = 0
+    for number in engine_speed_list:
+        if number < IDLING_ENGINE_THRESHOLD:
+            curr_streak = curr_streak + 1
+        else:
+            curr_streak = 0
+        idling_penalty = idling_penalty + curr_streak
+    score = 100. * (len(engine_speed_list) - idling_penalty) / len(engine_speed_list)
+    if score < 0:
+        score = 0.0
+    return score    
