930ef4efff8ade4751cde8fce31e23d3a167849e
Angela Yeung <ayyyeung@gmail.com>
2013-03-04 03:24:46 -0800

    added some database fields and metrics

--- prototype/prototype_app/views.py
+++ prototype/prototype_app/views.py
@@ -26,26 +26,27 @@ import csv, random, time
 #@login_required
 def index(request):
     facebook_profile = request.user.get_profile().get_facebook_profile()
-    pedal_force_list = []
-    mpg_list = []
-    ac_list = []
-    altitude_list = []   
-    engine_rpm_list = []
-    read_csv(pedal_force_list, mpg_list, ac_list, altitude_list, engine_rpm_list)
+    read_csv()
     #dates hard coded for now
     start_time = datetime.strptime('Thu Feb 28 23:13:32 PST 2013', '%a %b %d %H:%M:%S %Z %Y')
     end_time = datetime.strptime('Thu Feb 28 23:25:39 PST 2013', '%a %b %d %H:%M:%S %Z %Y')
     avg_speed = get_average(facebook_profile['id'], 'vehicle_speed', start_time, end_time)
-    avg_mpg = average_linear_value(mpg_list)
-    ac_score = score_air_conditioning(ac_list)
-    throttle_score = score_pedal_force(pedal_force_list)
-    avg_altitude = average_linear_value(altitude_list)
-    engine_score = score_idling(engine_rpm_list)
+    avg_mpg = get_average(facebook_profile['id'], 'mpg', start_time, end_time)
+    pedal_score = get_average(facebook_profile['id'], 'pedal_force', start_time, end_time)
+
+   # avg_altitude = get_average(facebook_profile['id'], 'altitude', start_time, end_time)
+    #ac_list = list(DriveData.values_list('air_conditioning', flat=True))
+    #ac_score = score_air_conditioning(ac_list)
+    #pedal_list = list(DriveData.objects.only("pedal_force"))
+    #throttle_score = score_pedal_force(pedal_list)
+
+    #engine_score = score_idling(engine_rpm_list)
 
     return render(request, 'dashboard.html', locals())
     #return render_to_response('index.html',  {'facebook_profile': facebook_profile}, context_instance=RequestContext(request))
 
-def read_csv(pedal_force_list, mpg_list, ac_list, altitude_list, engine_rpm_list):
+def read_csv():
+    #DriveData.objects.all().delete()
     cr = csv.reader(open('media/data/trackLog-2013-Feb-28_23-13-08.csv', 'rb'))
     counter = 0
     for row in cr:
@@ -54,11 +55,10 @@ def read_csv(pedal_force_list, mpg_list, ac_list, altitude_list, engine_rpm_list
             new_row.timestamp = datetime.strptime(row[0], '%a %b %d %H:%M:%S %Z %Y')
             new_row.vehicle_speed = row[4]
             new_row.air_conditioning = row[13] #data
-            pedal_force_list.append(row[18])
-            mpg_list.append(row[14])
-            ac_list.append(float(row[13])
-            altitude_list.append(float(row[6]))
-            engine_rpm_list.append(float(row[15]))
+            new_row.pedal_force = row[18]
+            new_row.mpg = row[14]
+            #new_row.altitude = row[6]
+            #new_row.engine_rpm = row[15]
             new_row.save()
         counter+=1
 
@@ -75,8 +75,9 @@ def generate_dashboard(request):
     return render(request, 'dashboard.html', {'vehicle_speed' : avg_speed})
 
 def get_average(username, metric, start_time, end_time):
+    formatted_metric = metric + "__avg"
     result = DriveData.objects.filter(timestamp__gte=start_time, timestamp__lte=end_time).aggregate(Avg(metric))
-    formatted_result = "{0:.2f}".format(result['vehicle_speed__avg'])
+    formatted_result = "{0:.2f}".format(result[formatted_metric])
     return formatted_result
 
 #####Temporary testing of some scoring functions #####
