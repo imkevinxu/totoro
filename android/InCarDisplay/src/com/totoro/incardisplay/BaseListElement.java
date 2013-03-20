bf867a573305be2d822d6764bb840b8f5b43339c
jliu12 <jliu12@stanford.edu>
2013-03-20 16:03:41 -0700

    Interface recognizes intent to launch listview

---
+++ android/InCarDisplay/src/com/totoro/incardisplay/BaseListElement.java
@@ -0,0 +1,53 @@
+package com.totoro.incardisplay;
+
+import android.graphics.drawable.Drawable;
+import android.view.View;
+import android.widget.BaseAdapter;
+
+public abstract class BaseListElement {
+	private Drawable icon;
+	private String text1;
+	private String text2;
+	private int requestCode;
+	private BaseAdapter adapter;
+	protected abstract View.OnClickListener getOnClickListener();
+
+	
+	public BaseListElement(Drawable icon, String text1, String text2, int requestCode) {
+	    super();
+	    this.icon = icon;
+	    this.text1 = text1;
+	    this.text2 = text2;
+	    this.requestCode = requestCode;
+	}
+	public void setAdapter(BaseAdapter adapter) {
+	    this.adapter = adapter;
+	}
+	
+	public int getRequestCode() {
+		return requestCode;
+	}
+	public String getText1() {
+		return text1;
+	}
+	public void setText1(String text1) {
+		this.text1 = text1;
+		if (adapter != null) {
+		    adapter.notifyDataSetChanged();
+		}
+	}
+	public String getText2() {
+		return text2;
+	}
+	public void setText2(String text2) {
+		this.text2 = text2;
+		if (adapter != null) {
+		    adapter.notifyDataSetChanged();
+		}
+	}
+
+
+	public Drawable getIcon() {
+		return this.icon;
+	}
+}
--- android/InCarDisplay/src/com/totoro/incardisplay/SelectionFragment.java
+++ android/InCarDisplay/src/com/totoro/incardisplay/SelectionFragment.java
@@ -1,11 +1,18 @@
 package com.totoro.incardisplay;
 
+import java.awt.List;
+import java.util.ArrayList;
+
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.facebook.Request;
@@ -21,106 +28,185 @@ public class SelectionFragment extends Fragment {
 	private ProfilePictureView profilePictureView;
 	private TextView userNameView;
 	private UiLifecycleHelper uiHelper;
+	private ListView listView;
+	private ArrayList<BaseListElement> listElements;
 	private static final int REAUTH_ACTIVITY_CODE = 100;
 
+	private class ActionListAdapter extends ArrayAdapter<BaseListElement> {
+		private ArrayList<BaseListElement> listElements;
+
+		public ActionListAdapter(Context context, int resourceId, 
+				ArrayList<BaseListElement> listElements) {
+			super(context, resourceId, listElements);
+			this.listElements = listElements;
+			// Set up as an observer for list item changes to
+			// refresh the view.
+			for (int i = 0; i < listElements.size(); i++) {
+				listElements.get(i).setAdapter(this);
+			}
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View view = convertView;
+			if (view == null) {
+				LayoutInflater inflater =
+						(LayoutInflater) getActivity()
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				view = inflater.inflate(R.layout.listitem, null);
+			}
+
+			BaseListElement listElement = listElements.get(position);
+			if (listElement != null) {
+				view.setOnClickListener(listElement.getOnClickListener());
+				ImageView icon = (ImageView) view.findViewById(R.id.icon);
+				TextView text1 = (TextView) view.findViewById(R.id.text1);
+				TextView text2 = (TextView) view.findViewById(R.id.text2);
+				if (icon != null) {
+					icon.setImageDrawable(listElement.getIcon());
+				}
+				if (text1 != null) {
+					text1.setText(listElement.getText1());
+				}
+				if (text2 != null) {
+					text2.setText(listElement.getText2());
+				}
+			}
+			return view;
+		}
+
+	}
+
+	private class PeopleListElement extends BaseListElement {
+
+		public PeopleListElement(int requestCode) {
+			super(getActivity().getResources().getDrawable(R.drawable.action_people),
+					getActivity().getResources().getString(R.string.action_people),
+					getActivity().getResources().getString(R.string.action_people_default),
+					requestCode);
+		}
+
+		@Override
+		protected View.OnClickListener getOnClickListener() {
+			return new View.OnClickListener() {
+				@Override
+				public void onClick(View view) {
+					// Do nothing for now
+				}
+			};
+		}
+	}
+
 	private Session.StatusCallback callback = new Session.StatusCallback() {
-	    @Override
-	    public void call(final Session session, final SessionState state, final Exception exception) {
-	        onSessionStateChange(session, state, exception);
-	    }
+		@Override
+		public void call(final Session session, final SessionState state, final Exception exception) {
+			onSessionStateChange(session, state, exception);
+		}
 	};
-	
+
 	@Override
 	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-	    super.onActivityResult(requestCode, resultCode, data);
-	    if (requestCode == REAUTH_ACTIVITY_CODE) {
-	        uiHelper.onActivityResult(requestCode, resultCode, data);
-	    }
+		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode == REAUTH_ACTIVITY_CODE) {
+			uiHelper.onActivityResult(requestCode, resultCode, data);
+		}
 	}
-	
+
 	@Override
 	public void onResume() {
-	    super.onResume();
-	    uiHelper.onResume();
+		super.onResume();
+		uiHelper.onResume();
 	}
 
 	@Override
 	public void onSaveInstanceState(Bundle bundle) {
-	    super.onSaveInstanceState(bundle);
-	    uiHelper.onSaveInstanceState(bundle);
+		super.onSaveInstanceState(bundle);
+		uiHelper.onSaveInstanceState(bundle);
 	}
 
 	@Override
 	public void onPause() {
-	    super.onPause();
-	    uiHelper.onPause();
+		super.onPause();
+		uiHelper.onPause();
 	}
 
 	@Override
 	public void onDestroy() {
-	    super.onDestroy();
-	    uiHelper.onDestroy();
+		super.onDestroy();
+		uiHelper.onDestroy();
 	}
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-	    super.onCreate(savedInstanceState);
-	    uiHelper = new UiLifecycleHelper(getActivity(), callback);
-	    uiHelper.onCreate(savedInstanceState);
+		super.onCreate(savedInstanceState);
+		uiHelper = new UiLifecycleHelper(getActivity(), callback);
+		uiHelper.onCreate(savedInstanceState);
 	}
-	
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, 
-	        ViewGroup container, Bundle savedInstanceState) {
-	    super.onCreateView(inflater, container, savedInstanceState);
-	    View view = inflater.inflate(R.layout.selection, 
-	            container, false);
-	 // Find the user's profile picture custom view
-	    profilePictureView = (ProfilePictureView) view.findViewById(R.id.selection_profile_pic);
-	    profilePictureView.setCropped(true);
+			ViewGroup container, Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		View view = inflater.inflate(R.layout.selection, 
+				container, false);
+		// Find the user's profile picture custom view
+		profilePictureView = (ProfilePictureView) view.findViewById(R.id.selection_profile_pic);
+		profilePictureView.setCropped(true);
 
-	    // Find the user's name view
-	    userNameView = (TextView) view.findViewById(R.id.selection_user_name);
-	    
-	    // Check for an open session
-	    Session session = Session.getActiveSession();
-	    if (session != null && session.isOpened()) {
-	        // Get the user's data
-	        makeMeRequest(session);
-	    }
-	    
-	    return view;
+		// Find the user's name view
+		userNameView = (TextView) view.findViewById(R.id.selection_user_name);
+
+		// Find the list view
+		listView = (ListView) view.findViewById(R.id.selection_list);
+
+		// Set up the list view items, based on a list of
+		// BaseListElement items
+		listElements = new ArrayList<BaseListElement>();
+		// Add an item for the friend picker
+		listElements.add(new PeopleListElement(0));
+		// Set the list view adapter
+		listView.setAdapter(new ActionListAdapter(getActivity(), 
+				R.id.selection_list, listElements));
+
+		// Check for an open session
+		Session session = Session.getActiveSession();
+		if (session != null && session.isOpened()) {
+			// Get the user's data
+			makeMeRequest(session);
+		}
+
+		return view;
 	}
-	
+
 	private void onSessionStateChange(final Session session, SessionState state, Exception exception) {
-	    if (session != null && session.isOpened()) {
-	        // Get the user's data.
-	        makeMeRequest(session);
-	    }
+		if (session != null && session.isOpened()) {
+			// Get the user's data.
+			makeMeRequest(session);
+		}
 	}
-	
+
 	private void makeMeRequest(final Session session) {
-	    // Make an API call to get user data and define a 
-	    // new callback to handle the response.
-	    Request request = Request.newMeRequest(session, 
-	            new Request.GraphUserCallback() {
-	        @Override
-	        public void onCompleted(GraphUser user, Response response) {
-	            // If the response is successful
-	            if (session == Session.getActiveSession()) {
-	                if (user != null) {
-	                    // Set the id for the ProfilePictureView
-	                    // view that in turn displays the profile picture.
-	                    profilePictureView.setProfileId(user.getId());
-	                    // Set the Textview's text to the user's name.
-	                    userNameView.setText(user.getName());
-	                }
-	            }
-	            if (response.getError() != null) {
-	                // Handle errors, will do so later.
-	            }
-	        }
-	    });
-	    request.executeAsync();
+		// Make an API call to get user data and define a 
+		// new callback to handle the response.
+		Request request = Request.newMeRequest(session, 
+				new Request.GraphUserCallback() {
+			@Override
+			public void onCompleted(GraphUser user, Response response) {
+				// If the response is successful
+				if (session == Session.getActiveSession()) {
+					if (user != null) {
+						// Set the id for the ProfilePictureView
+						// view that in turn displays the profile picture.
+						profilePictureView.setProfileId(user.getId());
+						// Set the Textview's text to the user's name.
+						userNameView.setText(user.getName());
+					}
+				}
+				if (response.getError() != null) {
+					// Handle errors, will do so later.
+				}
+			}
+		});
+		request.executeAsync();
 	} 
 }
