4757b782ccf944d0dbf7eaa3e1a0ad97f1afbc62
Huaiyu Wu <huaiyu.wu2010@gmail.com>
2013-04-15 22:46:42 -0700

    Added stopping, fuel usage, mpg, and trip length

--- android/InCarDisplay/src/com/totoro/incardisplay/simulations/DataGenerator.java
+++ android/InCarDisplay/src/com/totoro/incardisplay/simulations/DataGenerator.java
@@ -30,12 +30,8 @@ public class DataGenerator {
 		return data;
 	}
 	
-	// TODO(Nick): Add stopping feature.
 	// TODO(Nick): Latitude/longitude
-	// TODO(Nick): Fuel usage
-	// TODO(Nick): MPG
 	// TODO(Nick): Eco-score
-	// TODO(Nick): Trip-length
 	// TODO(Nick): Tell KX what units are
 	// TODO(Nick): Make output JSON
 	public void generateData(final int NUM_LINES) {
@@ -47,15 +43,25 @@ public class DataGenerator {
 		brakingPressure = 0;
 		pedalForce = 0;
 		altitude = 0;
-		double goalSpeed = 0;
-		int numSpeedTurnsLeft = 0;
+		fuelUsage = 0;
+		mpg = 0;
+		tripLength = 0;
+		double goalSpeed = 30 + 50 * rng.nextDouble();
+		int numSpeedTurnsLeft = 50+rng.nextInt(100);
+		int numStoppingTurns = 0;
 		data = new TimeSlice[NUM_LINES];
 		for(int line = 0; line < NUM_LINES; line++, numSpeedTurnsLeft--)	{
 			if(numSpeedTurnsLeft == 0)	{
-				goalSpeed = 80 * rng.nextDouble();
+				numStoppingTurns = 2 + rng.nextInt(8);
+				goalSpeed = 30 + 50 * rng.nextDouble();
 				numSpeedTurnsLeft = 50+rng.nextInt(100);
 			}
-			double accelInc = getNextAccel(goalSpeed, acceleration, currentSpeed);
+			boolean isStopped = false;
+			if(numStoppingTurns-- > 0)	{
+				isStopped = true;
+				numSpeedTurnsLeft++;
+			}
+			double accelInc = getNextAccel(isStopped, goalSpeed, acceleration, currentSpeed);
 			acceleration += accelInc;
 			if(currentSpeed + acceleration < 0)	{
 				currentSpeed = 0;
@@ -73,10 +79,23 @@ public class DataGenerator {
 			steering += getNextSteering(steering);
 			odometer += currentSpeed / 3600;
 			altitude = 20 * rng.nextGaussian();
+			fuelUsage += getFuelUsage(currentSpeed);
+			tripLength++;
+			mpg = odometer / fuelUsage;
 			data[line] = generateDatum();
 		}
 	}
 
+	private static final double FUEL_USAGE_SCALE = 0.0000001;
+	
+	/*
+	 * Amount of energy needed to travel per unit distance scales with
+	 * the square of speed.
+	 */
+	private double getFuelUsage(double currentSpeed)	{
+		return FUEL_USAGE_SCALE * currentSpeed * currentSpeed;
+	}
+	
 	private double currentSpeed;
 	private double acceleration;
 	private double airConditioning;
@@ -85,6 +104,9 @@ public class DataGenerator {
 	private double brakingPressure;
 	private double pedalForce;
 	private double altitude;
+	private double fuelUsage;
+	private double mpg;
+	private long tripLength;
 	
 	private TimeSlice generateDatum()	{
 		List<Datum> list = new ArrayList<Datum>();
@@ -96,6 +118,9 @@ public class DataGenerator {
 		list.add(new Datum("brakingPressure", brakingPressure));
 		list.add(new Datum("pedalForce", pedalForce));
 		list.add(new Datum("altitude", altitude));
+		list.add(new Datum("fuelUsage", fuelUsage));
+		list.add(new Datum("mpg", mpg));
+		list.add(new Datum("tripLength", tripLength));
 		return new TimeSlice(list);
 	}
 	
@@ -115,8 +140,11 @@ public class DataGenerator {
 
 	private static final double ACCEL_CORRECTION = 0.1;
 
-	private double getNextAccel(double goalSpeed, double acceleration, double currentSpeed) {
+	private double getNextAccel(boolean mustStop, double goalSpeed, double acceleration, double currentSpeed) {
 		double randomChange = ACCEL_SCALING_FACTOR * Math.abs(rng.nextGaussian()) * (goalSpeed - currentSpeed);
+		if(mustStop)	{
+			randomChange = Math.min(0, randomChange - 25);
+		}
 		acceleration -= ACCEL_CORRECTION * Math.abs(rng.nextGaussian()) * acceleration;
 		return randomChange;
 	}
