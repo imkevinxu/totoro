5404df1f9e5c71de99a3820b9aef203e6aa56ae2
Angela Yeung <ayyyeung@gmail.com>
2013-06-05 02:17:05 -0700

    skeleton for challenge

---
+++ bounce/src/com/authorwjf/bounce/ChallengeActivity.java
@@ -0,0 +1,29 @@
+package com.authorwjf.bounce;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class ChallengeActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.challenge);
+    }
+    
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+    
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+    
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
